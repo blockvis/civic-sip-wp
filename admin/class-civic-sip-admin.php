7b24172c2a0e5eadfd7198daf1a010934ca5353d
Alexey Rehov <alexey.rehov@gmail.com>
2017-09-27 15:32:31 +0300

    Admin code and asset clean up

--- admin/class-civic-sip-admin.php
+++ admin/class-civic-sip-admin.php
@@ -61,20 +61,6 @@ class Civic_Sip_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Civic_Sip_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Civic_Sip_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/civic-sip-admin.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
@@ -83,20 +69,6 @@ class Civic_Sip_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Civic_Sip_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Civic_Sip_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/civic-sip-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
