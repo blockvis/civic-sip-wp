e53400bda1e9244a36c03ab42352160910959ba1
Alexey Rehov <alexey.rehov@gmail.com>
2017-09-28 18:07:47 +0300

    Add registration confirmation modal

--- public/js/civic-sip-public.js
+++ public/js/civic-sip-public.js
@@ -37,6 +37,42 @@
                 console.log("Auth Response", response);
                 if (response.data.logged_in == true) {
                     document.location.href = civic_ajax.redirect_url;
+                } else {
+                    $('body').addClass('civic-hit-the-lights civic-no-scroll');
+                    var modal = $('div.civic-modal.civic-qrcode').addClass('civic-show');
+                    modal.find('iframe').remove();
+                    modal.find('.civic-content').append(
+                        // TODO: move rendering to php
+                        $('<div class="civic-onboarding">' +
+                            '<div class="civic-informant civic-form-modal">' +
+                            '  <div id="civic-retry-box" class="civic-top civic-switch">' +
+                            '      <h3>It looks like you are new to this site. <br> Please register using ' + response.data.email +
+                            '      </h3>' +
+                            '      <div class="civic-button-container two-up">' +
+                            '          <button type="button" id="civic-register" class="medium outline">Register</button>' +
+                            '          <div class="civic-help-links">' +
+                            '              <a id="civic-cancel" href="">Cancel</a>' +
+                            '          </div>' +
+                            '      </div>' +
+                            '  </div>' +
+                            '</div>' +
+                          '</div>')
+                    ).attr('style', ''); // Removes style height.
+
+                    $('#civic-register').on('click', function() {
+                        $.ajax({
+                            type: 'POST',
+                            url: civic_ajax.url, // TODO: chacnge to the new endpoint
+                            success: function () {
+                                document.location.href = civic_ajax.redirect_url;
+                            }
+                        });
+                    });
+
+                    $('#civic-cancel').on('click', function() {
+                        modal.removeClass('civic-show');
+                        $('body').removeClass('civic-hit-the-lights civic-no-scroll');
+                    });
                 }
             }
         });
