83a256f0542d6d032cf36b4ad6e80cdb48cf3f62
Alexey Rehov <alexey.rehov@gmail.com>
2017-10-05 16:05:48 +0300

    Remove overlay after successful ajax response on sendAuthCode

--- public/js/civic-sip-public.js
+++ public/js/civic-sip-public.js
@@ -13,16 +13,7 @@
         $('body').addClass('civic-hit-the-lights civic-no-scroll');
         var modal = $('div.civic-modal.civic-qrcode');
         modal.append(
-            '<div id="civic-loader" class="civic-loader large civic-switch">\n' +
-            '    <svg width="64px" height="64px" version="1.1" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n' +
-            '        <g id="keyhole" transform="translate(4.000000, 4.000000)">\n' +
-            '            <path d="M31.6750055,30.4909742 C34.1659012,29.271799 35.8812589,26.711394 35.8812589,23.750025 C35.8812589,19.607775 32.525102,16.250025 28.3848173,16.250025 C24.2445327,16.250025 20.8883758,19.607775 20.8883758,23.750025 C20.8883758,26.711493 22.6038482,29.2719702 25.094879,30.4910965 L25.094879,39.75 L31.6750055,39.75 L31.6750055,30.4909742 Z" fill="#FFFFFF"></path>\n' +
-            '        </g>\n' +
-            '        <g class="civic-circular" stroke="none" stroke-width="0" fill="none" fill-rule="evenodd">\n' +
-            '            <circle class="civic-path" cx="32" cy="32" r="28" stroke="#FFFFFF" stroke-width="8" fill="none" miterlimit="10"></circle>\n' +
-            '        </g>\n' +
-            '    </svg>\n' +
-            '</div>'
+            '<div id="civic-loader" class="civic-loader large civic-switch"><svg width="64px" height="64px" version="1.1" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="keyhole" transform="translate(4.000000, 4.000000)"><path d="M31.6750055,30.4909742 C34.1659012,29.271799 35.8812589,26.711394 35.8812589,23.750025 C35.8812589,19.607775 32.525102,16.250025 28.3848173,16.250025 C24.2445327,16.250025 20.8883758,19.607775 20.8883758,23.750025 C20.8883758,26.711493 22.6038482,29.2719702 25.094879,30.4910965 L25.094879,39.75 L31.6750055,39.75 L31.6750055,30.4909742 Z" fill="#FFFFFF"></path></g><g class="civic-circular" stroke="none" stroke-width="0" fill="none" fill-rule="evenodd"><circle class="civic-path" cx="32" cy="32" r="28" stroke="#FFFFFF" stroke-width="8" fill="none" miterlimit="10"></circle></g></svg></div>'
         ).addClass('civic-show');
         modal.find('.civic-window').hide();
 
@@ -55,9 +46,9 @@
                 if (response == 0) {
                     // Already logged in, reload the document
                     document.location.reload(true);
-                } else if (response.data.logged_in == true) {
+                } else if (response.data && response.data.logged_in == true) {
                     document.location.href = civic_ajax.redirect_url;
-                } else if (response.data.email && response.data.modal) {
+                } else if (response.data && response.data.email && response.data.modal) {
                     $('body').addClass('civic-hit-the-lights civic-no-scroll');
                     var modal = $('div.civic-modal.civic-qrcode').addClass('civic-show');
                     modal.find('iframe').remove();
@@ -87,6 +78,9 @@
                         modal.removeClass('civic-show');
                         $('body').removeClass('civic-hit-the-lights civic-no-scroll');
                     });
+                } else {
+                    $('div.civic-modal.civic-qrcode').removeClass('civic-show');
+                    $('body').removeClass('civic-hit-the-lights civic-no-scroll');
                 }
             }
         });
